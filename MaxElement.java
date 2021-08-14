bb36c618ec88cfbfba89724df9087094b71474cc
VIKRAMAN-MANIKANDAN <82633550+VIKRAMAN-MANIKANDAN@users.noreply.github.com>
2021-08-14 19:55:50 +0530

    Add files via upload

---
+++ MaxElement.java
@@ -0,0 +1,15 @@
+package max;
+import java.util.Scanner;
+public class MaxElement {
+  public static void main(String args[])
+  {
+	  Scanner sc=new Scanner(System.in);
+	  int[] arr=new int[25];
+	  int sum=0;
+	  for(int i=0;i<25;i++) {
+		  arr[i]=sc.nextInt();
+		  if(arr[i]>sum) sum=arr[i]; 
+	  }
+	  System.out.print(sum);
+  }
+}
